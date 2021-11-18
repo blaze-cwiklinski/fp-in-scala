ea7a4f4e2ddd3a3945dd6f7fd1959575895f1985
Błażej Ćwikliński <cwiklin.b@gmail.com>
2021-11-18 20:14:45 +0100

    Finished 'EXERCISE 2.3'.

---
+++ src/main/scala/training/ch02/ex03.scala
@@ -0,0 +1,26 @@
+package training
+
+object ex03 {
+
+
+    val moreThan = new Function2[Int, Int, Boolean] {
+        def apply(a: Int, b: Int) = a > b
+    }
+
+    def partial1[A,B,C](a: A, f: (A, B) => C): B => C = {
+        (b: B) => f(a, b)
+    }
+
+    def curry[A,B,C](f: (A, B) => C): A => (B => C) = {
+        a => b => f(a, b)
+    }
+
+    def sum(a: Int, b: Int) = a + b
+
+    def main(args: Array[String]): Unit = {
+        println(moreThan.apply(100, 10))
+        println(moreThan.apply(10, 100))
+
+        println(curry(sum)(5)(5))
+    }
+}
